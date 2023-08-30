6429f58d64f9bb804cf19b21ee8c8f564a9deae8
notmhmd <mhmdfrj.97@gmail.com>
2023-08-31 00:08:01 +0400

    update tailwind config

--- apps/portfolio/tailwind.config.js
+++ apps/portfolio/tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    // spacing: {
-    //   sm: '0.5rem',
-    //   md: '1rem',
-    //   lg: '1.5rem',
-    //   xl: '2rem',
-    // },
+    spacing: {
+      sm: '0.5rem',
+      md: '1rem',
+      lg: '1.5rem',
+      xl: '2rem',
+    },
     fontFamily:{
       'mono':['Montserrat'],
     }
